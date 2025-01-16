d2f9a5429e0297ec0805d4a0c43490962c4b4ccf
Abdelrahman-Mohamed121 <bedo.123bm@gmail.com>
2025-01-16 22:15:01 +0200

    Add logout button to navigate to login screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_app/providers/theme_provider.dart';
+import 'package:todo_app/screens/login_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        appBar: AppBar(
+      title: Text("My tasks"),
+      actions: [
+        IconButton(
+            onPressed: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => LoginScreen()),
+              );
+            },
+            icon: Icon(Icons.logout)),
+        IconButton(
+          onPressed: () {
+            context.read<ThemeProvider>().toggleTheme();
+          },
+          icon: Icon(context.read<ThemeProvider>().isDarkMode
+              ? Icons.light_mode_outlined
+              : Icons.dark_mode_outlined),
+        )
+      ],
+    ));
   }
 }
